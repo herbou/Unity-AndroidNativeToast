ca65e66d46ab0488b058bafbd87ec6dafa170fa0
hamza herbou <hamza95herbou@gmail.com>
2021-04-22 13:54:46 +0000

    Add files via upload

---
+++ Toast.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine ;
+
+public static class Toast {
+
+   public static void Show (string message) {
+      #if UNITY_EDITOR
+      Debug.Log ("Toast: " + message) ;
+
+      #elif UNITY_ANDROID
+      AndroidJavaClass unityPlayer = new AndroidJavaClass ("com.unity3d.player.UnityPlayer") ;
+      AndroidJavaObject appActivity = unityPlayer.GetStatic<AndroidJavaObject> ("currentActivity") ;
+
+      if (appActivity != null) {
+         AndroidJavaClass androidToast = new AndroidJavaClass ("android.widget.Toast") ;
+         appActivity.Call ("runOnUiThread", new AndroidJavaRunnable (() => {
+            AndroidJavaObject toastObject = androidToast.CallStatic <AndroidJavaObject> ("makeText", appActivity, message, 0) ;
+            toastObject.Call ("show") ;
+         })) ;
+      }
+
+      #endif
+   }
+
+}
